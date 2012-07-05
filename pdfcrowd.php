8f365a5d743896022601499ec922d561ea6c2aa8
pdfcrowd dev <info+github@pdfcrowd.com>
2012-07-05 23:54:28 +0200

    include the exception code in the exception message

--- pdfcrowd.php
+++ pdfcrowd.php
@@ -31,7 +31,16 @@
 //
 // Thrown when an error occurs.
 // 
-class PdfcrowdException extends Exception {}
+class PdfcrowdException extends Exception {
+    // custom string representation of object
+    public function __toString() {
+        if ($this->code) {
+            return "[{$this->code}] {$this->message}\n";
+        } else {
+            return "{$this->message}\n";
+        }
+    }
+}
 
 
 //
